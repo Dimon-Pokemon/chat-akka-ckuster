2c9d353a94e4581d4362e2dbe5f1ba3e61bd464f
Dimon-Pokemon <purumamuma@gmail.com>
2023-10-12 10:58:54 +0400

    Добавил комментарии к классу inputNameController.scala

--- src/main/scala/main/inputNameController.scala
+++ src/main/scala/main/inputNameController.scala
@@ -12,8 +12,12 @@ class inputNameController {
   @FXML
   var button: Button = null
 
-  private var mainChat: mainChat = null
+  private var mainChat: mainChat = null // Ссылка на основной класс приложения
 
+  /**
+   * Установка ссылки на основной класс приложения для доступ к его методам и атрибутам
+   * @param mainChat - ссылка на основной класс mainChat
+   */
   def setMainChat(mainChat: mainChat): Unit = {
     this.mainChat = mainChat
   }
@@ -26,7 +30,7 @@ class inputNameController {
   def closeEventHandler: EventHandler[WindowEvent] = new EventHandler[WindowEvent](){
     override def handle(event: WindowEvent): Unit = {
       mainChat.getInputNameStage.close()
-      System.exit(0)
+      System.exit(0) // Завершение работы приложения.
 
     }
   }
@@ -39,14 +43,19 @@ class inputNameController {
     override def handle(event: KeyEvent): Unit = {
       if(event.getCode.toString.equals("ENTER")) setUserName
     }
-  
+
+  /**
+   * Метод для получения введенного на GUI имени пользователя и
+   * установки его в качестве атрибута userName класса mainChat.
+   * Это имя будет отображаться на GUI.
+   */
   @FXML
   protected def setUserName: Unit = {
-    val textUserName = userName.getText
+    val textUserName = userName.getText // Получение введенного имени с GUI.
 
     if(!textUserName.isEmpty){
-      mainChat.userName = textUserName
-      mainChat.getInputNameStage.close()
+      mainChat.userName = textUserName // Установка введенного имени в атрибут userName.
+      mainChat.getInputNameStage.close() // Закрыаем окно ввода имени пользователя.
 
     }
   }
